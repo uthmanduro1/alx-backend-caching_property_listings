11e2273ddbe69652abcdbb8270781a4b9d26d8eb
Durosinlohun Uthman <udurosinlohun@gmail.com>
2025-07-12 02:01:36 +0100

    implemented redis in django

--- properties/views.py
+++ properties/views.py
@@ -1,7 +1,7 @@
 from django.views.decorators.cache import cache_page
 from django.http import JsonResponse
 from .models import Property
-from utils import get_all_properties
+from .utils import get_all_properties
 
 # @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
@@ -9,4 +9,4 @@ def property_list(request):
     #     'id', 'title', 'description', 'price', 'location', 'created_at'
     # )
     properties = get_all_properties()
-    return JsonResponse(list(properties), safe=False)
+    return JsonResponse({"data": list(properties)})
